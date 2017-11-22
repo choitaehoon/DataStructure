55fc58da5c2a40733b8477858cbf4890eae93bba
최태훈 <33123391+choitaehoon@users.noreply.github.com>
2017-11-22 17:25:49 +0900

    단말 노드는 되고 자식노드가 하나인 경우와 둘인 경우 하기

--- MyBinarySearchTree.java
+++ MyBinarySearchTree.java
@@ -1,4 +1,4 @@
-package lab9_1;
+package hw9_2;
 
 class MyBinarySearchTree {
 	// 트리의 루트 노드를 가리킬 인스턴스 변수 root 선언하고 null로 초기화
@@ -11,10 +11,9 @@ class MyBinarySearchTree {
 		{
 			this.key = key;
 		}
-
 		int key;
-		Node leftChild;
-		Node rightChild;
+		Node leftChild ;
+		Node rightChild ;
 	}
 
 	// (1) 전체조회를 위한 toString() 오버라이드
@@ -78,5 +77,199 @@ class MyBinarySearchTree {
 			postorder(p.rightChild);
 			System.out.print(p.key+" ");
 		}
+	}
+	//탐색하는 메소드
+	public boolean contains(int key)
+	{
+		return contains(key, root);
+	}
+	//정보은닉 ->외부에게 공개 x
+	private boolean contains(int key, Node temp)
+	{
+		if(temp == null)
+			return false;
+		else
+		{
+			if(key == temp.key) 
+				return true;
+			else if(key< temp.key)
+				return contains(key, temp.leftChild);	
+			else
+				return contains(key, temp.rightChild);
+		}
+	}
+	
+	//최대값 출력하는 메소드
+	public int max()
+	{
+		 return max(root);
+	}
+	//정보 은닉 (확인하기)
+	private int max(Node temp) throws NullPointerException
+	{
+		if(temp == null)
+		{
+			throw new NullPointerException("공백 이진탐색 트리 입니다");
+		}
+		else
+		{
+			int data = 0;
+			if(temp.rightChild != null)
+			data = max(temp.rightChild);
+			return temp.key > data ? temp.key : data;	
+		}
+	}
+	
+	//삽입 하기
+	public boolean add(int key)
+	{
+		return add(key, root);
+	}
+	
+	//반복알고리즘 사용하기
+	private boolean add(int key, Node temp)
+	{
+		Node newNode = new Node(key);
+		if(temp == null)
+		{
+			root = newNode;
+			return true;
+		}
+		else
+		{
+			while(temp != null)
+			{
+				if(temp.key == key)
+					return false;	
+
+				else if(temp.key < key)
+				{
+					if(temp.rightChild == null)
+					{
+						temp.rightChild = newNode;
+						return true;
+					}	
+					else
+						temp = temp.rightChild;	
+				}
+				
+				else
+				{
+					if(temp.leftChild == null)
+					{
+						temp.leftChild = newNode;
+						return true;
+					}
+					else
+						temp = temp.leftChild;	
+				}
+			}
+			return false;
+		}
+	}
+	
+	//삭제 메소드
+	public boolean remove(int key)
+	{
+		return remove(key,root);
+	}
+	
+	//정보 은닉 메소드 
+	private boolean remove(int key, Node temp)
+	{
+		if(temp == null) //아예 원소가 없을때
+			return false;
+		
+		else //원소가 있을 경우   삭제는 경우가 3가지 있는데 1.단말노드 2.자식노드가 하나인 경우 3.자식 노드가 두개인 경우
+		{	
+			cases(key);
+			return true;
+		}
+		
+	}
+	
+	// 삭제할때 경우의 세가지수 정의하기 1.단말노드 2.자식 노드가 하나인 경우 3.자식 노드가 두개 인경우(후계자를 선택해야 하는데 나는 왼쪽서브트리 후계자 선택할 예정)
+	private void cases(int key)
+	{
+		Node temp = root; //삭제할 노드를 탐색하는 참조변수
+		Node parent = null;
+		
+		if(temp.key == key && temp.leftChild == null && temp.rightChild== null) //루트이면서 단말 노드 인경우
+		{
+			root = null;
+			return ;
+		}
+		
+			while(temp != null)
+			{
+				
+				if(temp.key < key) //탐색 할때 오른쪽으로 가는 곳
+				{
+					
+					parent = temp;
+					temp = temp.rightChild;
+					if(temp.key == key && (temp.leftChild == null && temp.rightChild == null)) // 단말 노드 인경우
+					{
+						if(parent.leftChild == temp)
+							parent.leftChild = null;
+						else
+							parent.rightChild = null;
+						return ;
+					}
+					
+					else if(temp.key == key && (temp.leftChild == null || temp.rightChild == null)) //자식 노드가 하나 인경우 (오른쪽)
+					{
+						if(parent.leftChild == temp)
+							parent.leftChild = temp.rightChild;
+						else
+							parent.rightChild = temp.rightChild;
+					}
+						
+					else if(temp.key == key && (temp.leftChild != null && temp.rightChild != null))// 자식 노드가 두개 인 경우
+					{
+						int data = 0;
+						if(temp.leftChild != null)
+							data = max(temp.leftChild);
+						temp.key = data;
+						//마지막으로 temp.leftChild = remove(temp.leftChild, temp.key);하면 되는데 고민해봐야함
+						return ;
+					}
+						
+						
+				}
+				else //탐색할때 왼쪽으로 가는 곳
+				{
+					
+					parent = temp;
+					temp = temp.leftChild;
+					if(temp.key == key && (temp.leftChild == null && temp.rightChild == null))// 단말 노드 인경우
+					{
+						if(parent.leftChild == temp)
+							parent.leftChild = null;
+						else
+							parent.rightChild = null;
+						return ;
+					}
+					else if(temp.key == key && (temp.leftChild == null || temp.rightChild != null)) //자식 노드가 하나 인경우 (왼쪽)
+					{
+						if(parent.leftChild == temp)
+							parent.leftChild = temp.leftChild;
+						else
+							parent.rightChild = temp.leftChild;
+					}
+					else if(temp.key == key && (temp.leftChild != null && temp.rightChild != null))// 자식 노드가 두개 인 경우
+					{
+						int data = 0;
+						if(temp.leftChild != null)
+							data = max(temp.leftChild);
+						temp.key = data;
+						return ;
+					}
+					
+				}
+				
+			}
+		
+		
 	}
 }
--- MyBinarySearchTreeTest.java
+++ MyBinarySearchTreeTest.java
@@ -1,10 +1,10 @@
-package lab9_1;
+package hw9_2;
 
 import java.util.Scanner;
 
 public class MyBinarySearchTreeTest {
 	public static void main(String[] args) {
-		System.out.println("lab9_1 : 최태훈");
+		System.out.println("hw9_2 : 최태훈");
 
 		// 공백 이진탐색트리 tree를 생성
 		MyBinarySearchTree tree = new MyBinarySearchTree();
@@ -16,7 +16,7 @@ public class MyBinarySearchTreeTest {
 
 		// 종료를 선택할 때까지 반복하여 메뉴를 제공하고 그에 맞는 트리 연산을 수행
 		do {
-			System.out.print("1:전체조회 2:재귀삽입 3:후위순회 9:종료 --->");
+			System.out.print("1:전체조회 2:재귀삽입 3:후위순회 4.검색 수행 5.최대값 출력 6.삽입수행(반복알고리즘) 7.삭제  9:종료 --->");
 			menu = scan.nextInt();
 			switch(menu) {
 			case 1:
@@ -33,38 +33,38 @@ public class MyBinarySearchTreeTest {
 				System.out.println("후위순회를 수행합니다.");
 				tree.printPostorder();		 		// (3)
 				break;
-//			case 4:
-//				System.out.println("검색을 수행합니다.");
-//				System.out.print("검색할 정수 입력:");
-//				item = scan.nextInt();
-//				if(tree.contains(item))				// (4)
-//					System.out.println("검색 성공");
-//				else
-//					System.out.println("검색 실패");
-//				break;
-//			case 5:
-//				System.out.println("최대값 조회를 수행합니다.");
-//				System.out.println(tree.max());			// (5)
-//				break;
-//			case 6:
-//				System.out.println("반복 알고리즘을 이용한 삽입을 수행합니다.");
-//				System.out.print("삽입할 정수 입력:");
-//				item = scan.nextInt();
-//				if(tree.add(item))				// (6)
-//					System.out.println("삽입 성공");
-//				else
-//					System.out.println("삽입 실패");
-//				break;
-//			case 7:
-//				System.out.println("삭제를 수행합니다.");
-//				System.out.print("삭제할 정수 입력:");
-//				item = scan.nextInt();
-//				if(tree.remove(item))				// (7)
-//					System.out.println("삭제 성공");
-//				else
-//					System.out.println("삭제 실패");
-//
-//				break;
+			case 4:
+				System.out.println("검색을 수행합니다.");
+				System.out.print("검색할 정수 입력:");
+				item = scan.nextInt();
+				if(tree.contains(item))				// (4)
+					System.out.println("검색 성공");
+				else
+					System.out.println("검색 실패");
+				break;
+			case 5:
+				System.out.println("최대값 조회를 수행합니다.");
+				System.out.println(tree.max());			// (5)
+				break;
+			case 6:
+				System.out.println("반복 알고리즘을 이용한 삽입을 수행합니다.");
+				System.out.print("삽입할 정수 입력:");
+				item = scan.nextInt();
+				if(tree.add(item))				// (6)
+					System.out.println("삽입 성공");
+				else
+					System.out.println("삽입 실패");
+				break;
+			case 7:
+				System.out.println("삭제를 수행합니다.");
+				System.out.print("삭제할 정수 입력:");
+				item = scan.nextInt();
+				if(tree.remove(item))				// (7)
+					System.out.println("삭제 성공");
+				else
+					System.out.println("삭제 실패");
+
+				break;
 			case 9:
 				System.out.println("프로그램을 종료합니다.");
 				break;
